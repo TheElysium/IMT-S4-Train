3948e77c70833d574e4be0a201c7bc9f14e08b34
Mael29000 <maelsuard@orange.fr>
2023-05-05 10:35:01 +0200

    Fusion du déplacement souris + zoom

--- src/interaction-grid.js
+++ src/interaction-grid.js
@@ -1,178 +1,271 @@
-import {getCell, getCellPosition, visualizePath} from "./utils/utils.js";
-import {StraightRail} from "./models/straightRail.js";
-import {TurnRail} from "./models/turnRail.js";
-import {Station} from "./models/station.js";
+import { getCell, getCellPosition, visualizePath } from "./utils/utils.js";
+import { StraightRail } from "./models/straightRail.js";
+import { TurnRail } from "./models/turnRail.js";
+import { Station } from "./models/station.js";
+import { gridWidth, gridHeight } from "./main.js";
 
 export class InteractionGrid {
-    constructor(width, height, container, gameGrid) {
-        this.width = width;
-        this.height = height;
-        this.container = container;
-        this.gameGrid = gameGrid;
+  constructor(width, height, container, gameGrid) {
+    this.width = width;
+    this.height = height;
+    this.container = container;
+    this.gameGrid = gameGrid;
 
-        this.initGrid();
-        this.addEventListeners();
-        this.initMovement();
+    this.initGrid();
+    this.addEventListeners();
+    this.initMovement();
+  }
+
+  initGrid() {
+    for (let x = 0; x < this.height; x++) {
+      for (let y = 0; y < this.width; y++) {
+        const cell = document.createElement("div");
+        cell.classList.add("c-wrapper__grid-container__interaction-grid__cell");
+        cell.dataset.x = x;
+        cell.dataset.y = y;
+
+        this.container.appendChild(cell);
+        cell.addEventListener("mouseenter", (event) =>
+          this.movingOnGridWithMouse(event)
+        );
+      }
     }
+  }
 
-    initGrid() {
-        for (let x = 0; x < this.height; x++) {
-            for (let y = 0; y < this.width; y++) {
-                const cell = document.createElement("div");
-                cell.classList.add("c-wrapper__grid-container__interaction-grid__cell");
-                cell.dataset.x = x;
-                cell.dataset.y = y;
+  addEventListeners() {
+    addEventListener("keydown", (event) => this.keyDownDetected(event));
+    this.container.addEventListener("mousedown", (event) =>
+      this.handleClick(event)
+    );
+    this.container.addEventListener("wheel", (event) => {
+      console.log("zoomHandler");
 
-                this.container.appendChild(cell);
-                cell.addEventListener("mouseenter", (event) => this.movingOnGridWithMouse(event));
-            }
-        }
-    }
+      // ONLY IF CTRL IS PRESSED
+      if (event.ctrlKey) {
+        // determine the direction of the scroll
+        const direction = event.deltaY > 0 ? -1 : 1;
 
-    addEventListeners() {
-        addEventListener("keydown", (event) => this.keyDownDetected(event));
-        this.container.addEventListener("mousedown", (event) => this.handleClick(event));
-        this.container.addEventListener("wheel", () => {
-            const position = getCellPosition(this.activeCell);
-            this.gameGrid.rotateRail(position);
-        });
-    }
+        // redifined the size of the grid template columns and rows
+        // first get the current size
+        const currentSize = parseInt(
+            this.container.style.gridTemplateColumns
+              .split(" ")[1]
+              .replace("px)", "")
+          ),
+          // then calculate the new size
+          newSize = currentSize + direction * 2;
+        console.log("CTRL PRESSED", this.container, currentSize, newSize);
 
-    handleClick(event) {
-        if(event.button !== 0) {
-            return;
-        }
-        const position = getCellPosition(this.activeCell);
-        const gameGridCell = this.gameGrid.grid[position.x][position.y];
-        if(gameGridCell === null){
-            this.menuRail(position, event.pageX, event.pageY);
-        }
-        else if(gameGridCell instanceof StraightRail || gameGridCell instanceof TurnRail){
-            this.gameGrid.removeRail(position);
-        }
-    }
+        const gameGridContainer = document.querySelector(
+          ".c-wrapper__grid-container__game-grid"
+        );
 
-    keyDownDetected(e){
-        if (e.code === "ArrowRight" || e.code === "ArrowLeft" || e.code === "ArrowUp" || e.code === "ArrowDown") {
-            this.movingOnGridWithKeyboard(e.code)
-        }
-        else {
-            console.log("Keypress not supported yet.");
-        }
-    }
+        console.log(gameGridContainer);
 
-    movingOnGridWithKeyboard(key){
-        const xyCell = getCellPosition(this.activeCell);
-        let cell;
+        // if the new size is between 50 and 200, update the size
+        if (newSize >= 50 && newSize <= 300) {
+          this.container.style.gridTemplateColumns = `repeat(${gridWidth}, ${newSize}px)`;
+          this.container.style.gridTemplateRows = `repeat(${gridHeight}, ${newSize}px)`;
 
-        switch (key) {
-            case "ArrowRight":
-                cell = getCell(xyCell.x, xyCell.y+1, this.container);
-                break;
-            case "ArrowLeft":
-                cell = getCell(xyCell.x, xyCell.y-1, this.container);
-                break;
-            case "ArrowUp":
-                cell = getCell(xyCell.x-1, xyCell.y, this.container);
-                break;
-            case "ArrowDown":
-                cell = getCell(xyCell.x+1, xyCell.y, this.container);
-                break;
-        }
-        cell ? this.updateActiveCell(cell) : console.log('Trying to go out of grid');
-    }
-
-    movingOnGridWithMouse(event){
-        let cell = event.target;
-        if(!Array.from(cell.classList).includes("c-wrapper__grid-container__interaction-grid__cell")){
-            console.log("Not on the grid");
-            return;
+          gameGridContainer.style.gridTemplateColumns = `repeat(${gridWidth}, ${newSize}px)`;
+          gameGridContainer.style.gridTemplateRows = `repeat(${gridHeight}, ${newSize}px)`;
         }
 
-        this.updateActiveCell(cell);
+        // prevent the page from scrolling
+        event.preventDefault();
+        return;
+      }
+
+      const position = getCellPosition(this.activeCell);
+      this.gameGrid.rotateRail(position);
+    });
+  }
+
+  handleClick(event) {
+    if (event.button !== 0) {
+      return;
+    }
+    if (event.ctrlKey) {
+      const container = document.querySelector(".c-wrapper__grid-container");
+
+      let pos = {
+        // The current scroll
+        left: container.scrollLeft,
+        top: container.scrollTop,
+        // Get the current mouse position
+        x: event.clientX,
+        y: event.clientY,
+      };
+
+      container.style.cursor = "grabbing";
+      container.style.userSelect = "none";
+
+      const mouseMoveHandler = (event) => {
+        // How far the mouse has been moved
+        const dx = event.clientX - pos.x;
+        const dy = event.clientY - pos.y;
+
+        const container = document.querySelector(".c-wrapper__grid-container");
+
+        // Scroll the element
+        container.scrollTop = pos.top - dy;
+        container.scrollLeft = pos.left - dx;
+      };
+
+      const mouseUpHandler = (event) => {
+        document.removeEventListener("mousemove", mouseMoveHandler);
+        document.removeEventListener("mouseup", mouseUpHandler);
+
+        container.style.removeProperty("cursor");
+        container.style.removeProperty("user-select");
+      };
+
+      document.addEventListener("mouseup", mouseUpHandler);
+      document.addEventListener("mousemove", mouseMoveHandler);
+      return;
+    }
+    const position = getCellPosition(this.activeCell);
+    const gameGridCell = this.gameGrid.grid[position.x][position.y];
+    if (gameGridCell === null) {
+      this.menuRail(position, event.pageX, event.pageY);
+    } else if (
+      gameGridCell instanceof StraightRail ||
+      gameGridCell instanceof TurnRail
+    ) {
+      this.gameGrid.removeRail(position);
+    }
+  }
+
+  keyDownDetected(e) {
+    if (
+      e.code === "ArrowRight" ||
+      e.code === "ArrowLeft" ||
+      e.code === "ArrowUp" ||
+      e.code === "ArrowDown"
+    ) {
+      this.movingOnGridWithKeyboard(e.code);
+    } else {
+      console.log("Keypress not supported yet.");
+    }
+  }
+
+  movingOnGridWithKeyboard(key) {
+    const xyCell = getCellPosition(this.activeCell);
+    let cell;
+
+    switch (key) {
+      case "ArrowRight":
+        cell = getCell(xyCell.x, xyCell.y + 1, this.container);
+        break;
+      case "ArrowLeft":
+        cell = getCell(xyCell.x, xyCell.y - 1, this.container);
+        break;
+      case "ArrowUp":
+        cell = getCell(xyCell.x - 1, xyCell.y, this.container);
+        break;
+      case "ArrowDown":
+        cell = getCell(xyCell.x + 1, xyCell.y, this.container);
+        break;
+    }
+    cell
+      ? this.updateActiveCell(cell)
+      : console.log("Trying to go out of grid");
+  }
+
+  movingOnGridWithMouse(event) {
+    let cell = event.target;
+    if (
+      !Array.from(cell.classList).includes(
+        "c-wrapper__grid-container__interaction-grid__cell"
+      )
+    ) {
+      console.log("Not on the grid");
+      return;
     }
 
-    updateActiveCell(cell) {
-        this.activeCell.classList.remove("active");
-        this.activeCell = cell;
-        this.activeCell.classList.add("active");
-    }
+    this.updateActiveCell(cell);
+  }
 
-    initMovement(){
-        this.activeCell = getCell(0, 0, this.container);
-        this.activeCell.classList.add("active");
-    }
+  updateActiveCell(cell) {
+    this.activeCell.classList.remove("active");
+    this.activeCell = cell;
+    this.activeCell.classList.add("active");
+  }
 
-    menuRail(position, width, height) {
-        const menu = document.getElementById('circle');
+  initMovement() {
+    this.activeCell = getCell(0, 0, this.container);
+    this.activeCell.classList.add("active");
+  }
 
-        this.showMenu(menu, width, height);
+  menuRail(position, width, height) {
+    const menu = document.getElementById("circle");
 
-        const addStraightRail = document.getElementById('add-straight-rail');
-        // const addSwitchRail = document.getElementById('add-switch-rail');
-        const addTurnRail = document.getElementById('add-turn-rail');
+    this.showMenu(menu, width, height);
 
-        addStraightRail.onmouseup = () => {
-            this.gameGrid.addStraightRail(position);
-            this.hideMenu(menu);
-        };
+    const addStraightRail = document.getElementById("add-straight-rail");
+    // const addSwitchRail = document.getElementById('add-switch-rail');
+    const addTurnRail = document.getElementById("add-turn-rail");
 
-        addTurnRail.onmouseup = () => {
-            this.gameGrid.addTurnRail(position);
-            this.hideMenu(menu);
-        };
+    addStraightRail.onmouseup = () => {
+      this.gameGrid.addStraightRail(position);
+      this.hideMenu(menu);
+    };
 
-        menu.onmouseup = () => {
-            this.hideMenu(menu);
-        };
+    addTurnRail.onmouseup = () => {
+      this.gameGrid.addTurnRail(position);
+      this.hideMenu(menu);
+    };
 
-        menu.onmouseleave = () => {
-            this.hideMenu(menu);
-        }
-    }
-    showMenu(menu, x, y) {
-        menu.style.display = 'flex';
-        const width = menu.offsetWidth;
-        const height = menu.offsetHeight;
+    menu.onmouseup = () => {
+      this.hideMenu(menu);
+    };
 
-        menu.style.left = x - (width/2) + 'px';
-        menu.style.top = y - (height/2) + 'px';
-    }
+    menu.onmouseleave = () => {
+      this.hideMenu(menu);
+    };
+  }
+  showMenu(menu, x, y) {
+    menu.style.display = "flex";
+    const width = menu.offsetWidth;
+    const height = menu.offsetHeight;
 
-    hideMenu(menu) {
-        menu.style.display = 'none';
-    }
+    menu.style.left = x - width / 2 + "px";
+    menu.style.top = y - height / 2 + "px";
+  }
 
-    play(timestamp) {
-        this.gameGrid.play(timestamp);
-    }
+  hideMenu(menu) {
+    menu.style.display = "none";
+  }
 
-    pause(timestamp) {
-        this.gameGrid.pause(timestamp);
-    }
+  play(timestamp) {
+    this.gameGrid.play(timestamp);
+  }
 
-    reset() {
-        this.gameGrid.reset();
-    }
+  pause(timestamp) {
+    this.gameGrid.pause(timestamp);
+  }
 
-    accelerate() {
-        this.gameGrid.accelerate();
-    }
+  reset() {
+    this.gameGrid.reset();
+  }
 
-    decelerate() {
-        this.gameGrid.decelerate();
-    }
+  accelerate() {
+    this.gameGrid.accelerate();
+  }
 
-    hideGrid() {
-        this.container.style.display = "none";
-    }
+  decelerate() {
+    this.gameGrid.decelerate();
+  }
 
-    showGrid() {
-        this.container.style.display = "grid";
-    }
+  hideGrid() {
+    this.container.style.display = "none";
+  }
 
-    isPlaying() {
-        return this.gameGrid.playing;
-    }
+  showGrid() {
+    this.container.style.display = "grid";
+  }
 
+  isPlaying() {
+    return this.gameGrid.playing;
+  }
 }
